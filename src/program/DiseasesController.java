3023c7e3498d30b7e6c94331fd769e8ab55a4414
AliKemalUzar <alikemaluzar@gmail.com>
2020-06-03 01:40:13 +0200

    Diseases

--- src/program/DiseasesController.java
+++ src/program/DiseasesController.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class BeschikbaarhedenController {
+public class DiseasesController {
     public void naHome(ActionEvent event) throws IOException {
         Parent showHome = FXMLLoader.load(getClass().getResource("homescreen.fxml"));
         Scene showHomeScene = new Scene(showHome);
@@ -17,15 +17,6 @@ public class BeschikbaarhedenController {
         //pakt stage informatie
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(showHomeScene);
-        window.show();
-    }
-    public void naDiseases(ActionEvent event) throws IOException {
-        Parent showDiseases = FXMLLoader.load(getClass().getResource("Diseases.fxml"));
-        Scene showDiseasesScene = new Scene(showDiseases);
-
-        //pakt stage informatie
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(showDiseasesScene);
         window.show();
     }
 }
