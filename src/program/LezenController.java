5be28cb779238585aff5d78f5ba4d9e7fa4e08a9
ME <flippertata12@gmail.com>
2020-06-08 15:36:51 +0200

    Lezen

--- src/program/LezenController.java
+++ src/program/LezenController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
@@ -13,6 +14,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LezenController {
+    private int points = 0;
+    @FXML
+    private Button submitButton;
+    @FXML
+    private Label feedback;
     @FXML
     private Label Vraag1;
     @FXML
@@ -37,19 +43,29 @@ public class LezenController {
         window.show();
     }
     public void submit(){
-        if(V1A1.isSelected() == true)
-            Vraag1.setText(Vraag1.getText()+"   Correct");
+        if(V1A1.isSelected() == true) {
+            Vraag1.setText(Vraag1.getText() + "   Correct");
+            points =points +4;
+        }
         else
             Vraag1.setText(Vraag1.getText()+"   Fout");
-        if(V2A2.isSelected() == true)
+        if(V2A2.isSelected() == true){
             Vraag2.setText(Vraag2.getText()+"   Correct");
+            points =points +3;
+        }
         else
             Vraag2.setText(Vraag2.getText()+"   Fout");
-        if(V3A3.isSelected() == true)
+        if(V3A3.isSelected() == true){
             Vraag3.setText(Vraag3.getText()+"   Correct");
+            points =points +3;
+        }
         else
             Vraag3.setText(Vraag3.getText()+"   Fout");
 
+        submitButton.setVisible(false);
+        feedback.setText("Your final score is :"+points);
+
     }
+
 
 }
