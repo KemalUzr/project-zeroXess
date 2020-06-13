07702e096247d226a0c8971a9076a73cd1e97a97
JED-HHS <18065864@student.hhs.nl>
2020-06-13 13:26:03 +0200

    check

---
+++ src/program/Eyes.java
@@ -0,0 +1,10 @@
+package program;
+
+public class Eyes extends Specialization{
+    private String name = "Eyes";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/program/Skin.java
@@ -0,0 +1,10 @@
+package program;
+
+public class Skin extends Specialization {
+    private String name = "Skin";
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
