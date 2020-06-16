68c2107bcec63ce44b81bb0f5d2501dce9f26ca7
Thimo <1026716@idcollege.nl>
2020-06-16 18:41:20 +0200

    Made a lot

---
+++ src/program/Opdracht.java
@@ -0,0 +1,17 @@
+package program;
+
+public class Opdracht {
+    String input;
+
+//    public Opdracht(String input) {
+//        this.input = input;
+//    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
