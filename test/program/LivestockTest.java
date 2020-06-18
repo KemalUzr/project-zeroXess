0855d0c325ea26eb35e5123d32e0f6bee26bd609
mehmet70 <50658677+mehmet70@users.noreply.github.com>
2020-06-19 00:28:13 +0200

    Comment
    Comments toegevoegd aan de Livestock test

--- test/program/LivestockTest.java
+++ test/program/LivestockTest.java
@@ -16,18 +16,18 @@ class LivestockTest {
 
     @Test
     void saveStock() throws IOException {
-        String voorbeeldinvoerveld = "13";
-        Mammal test = new Mammal("12", "33", "Dog");
-        test.setAmount(voorbeeldinvoerveld);
-        assertEquals(test.getAmount(), voorbeeldinvoerveld);
+        String voorbeeldinvoerveld = "13"; //dit moet een invoerveld voorstellen
+        Mammal test = new Mammal("12", "33", "Dog"); //aanmaken van nieuwe mamal
+        test.setAmount(voorbeeldinvoerveld);    //gebruikt de setAmount functie om de amount van test te veranderen naar de invoerveld.
+        assertEquals(test.getAmount(), voorbeeldinvoerveld); //test of de amount van test gelijk is aan
         try {
             BufferedWriter writercowA = new BufferedWriter(new FileWriter("testAmount.txt"));
-            writercowA.write(voorbeeldinvoerveld);
+            writercowA.write(voorbeeldinvoerveld);  //zet de invoerveld waarde in de testAmount.txt
             writercowA.close();
         }catch (IOException e) {
             e.printStackTrace();
         }
-        assertEquals(test.getAmount(), getValue("testAmount"));
+        assertEquals(test.getAmount(), getValue("testAmount")); //Test of de waarde van de amount die in test zit gelijk is aan de waarde in de .txt bestand
     }
 
     public String getValue(String specific) throws IOException {
