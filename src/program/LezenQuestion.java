513fa5b2c16b5d08160350f6c6de104f9458f9e4
ME <flippertata12@gmail.com>
2020-06-20 15:49:36 +0200

    Profiel test volgens het test ontwerp

---
+++ src/program/LezenQuestion.java
@@ -0,0 +1,13 @@
+package program;
+
+public class LezenQuestion {
+    String question;
+    String answer;
+    int points;
+
+    public LezenQuestion(String question, String answer, int points){
+        this.question = question;
+        this.answer = answer;
+        this.points = points;
+    }
+}
