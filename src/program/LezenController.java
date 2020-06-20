d3327b5a0b2f39f2ab7c1db366088950cc3752e7
ME <flippertata12@gmail.com>
2020-06-20 15:36:35 +0200

    Lezen volgens feedback van meneer Krogt

--- src/program/LezenController.java
+++ src/program/LezenController.java
@@ -6,33 +6,58 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
-public class LezenController {
-    public int points = 0;
+public class LezenController{
+    public int totalPoints = 0;
+    @FXML
+    private TextArea QuestionField;
+    @FXML
+    private TextField AnswerField;
+    @FXML
+    private Button startButton;
+    @FXML
+    private Label Feedback;
     @FXML
     private Button submitButton;
-    @FXML
-    private Label feedback;
-    @FXML
-    private Label Vraag1;
-    @FXML
-    private Label Vraag2;
-    @FXML
-    private Label Vraag3;
 
-    @FXML
-    public RadioButton correctAnswer1;
-    @FXML
-    public RadioButton correctAnswer2;
-    @FXML
-    public RadioButton correctAnswer3;
+    public String Q1 = "Where was the mother chicken sitting ?\n"
+            +"(A) On her nest\n(B) On a rock\n(C) On the street";
+    public String Q2 = "What color were her feathers ?\n"
+            +"(A) Black\n(B) White\n(C) Yellow";
+    public String Q3 = "What happend to the eggs ?\n"
+            +"(A) It chcanged color\n(B) Nothing\n(C) It cracked open";
 
+    LezenQuestion[] questions = {
+            new LezenQuestion(Q1, "A", 4),
+            new LezenQuestion(Q2, "B", 3),
+            new LezenQuestion(Q3, "C", 3)
+    };
+    int i = 0;
+public void startTest(){
+        QuestionField.setText(questions[i].question);
+        startButton.setVisible(false);
+    }
+public void submit(){
+        String text = AnswerField.getText();
+        if (text.equals(questions[i].answer)) {
+            Feedback.setText("correct");
+            totalPoints= totalPoints+questions[i].points;
+        }
+        else
+            Feedback.setText("incorrect");
+        if(i<2)
+            i++;
+        else {
+            Feedback.setText("Your final score is :" + totalPoints);
+            submitButton.setVisible(false);
+        }
+        QuestionField.setText(questions[i].question);
+        AnswerField.clear();
+
+}
     public void toHomescreen(ActionEvent event) throws IOException {
         Parent tohome  = FXMLLoader.load(getClass().getResource("homescreen.fxml"));
         Scene homeScene = new Scene(tohome);
@@ -42,30 +67,4 @@ public class LezenController {
         window.setScene(homeScene);
         window.show();
     }
-    public void submit(){
-        if(correctAnswer1.isSelected() == true) {
-            Vraag1.setText(Vraag1.getText() + "   Correct");
-            points =points +4;
-        }
-        else
-            Vraag1.setText(Vraag1.getText()+"   Fout");
-        if(correctAnswer2.isSelected() == true){
-            Vraag2.setText(Vraag2.getText()+"   Correct");
-            points =points +3;
-        }
-        else
-            Vraag2.setText(Vraag2.getText()+"   Fout");
-        if(correctAnswer3.isSelected() == true){
-            Vraag3.setText(Vraag3.getText()+"   Correct");
-            points =points +3;
-        }
-        else
-            Vraag3.setText(Vraag3.getText()+"   Fout");
-
-        submitButton.setVisible(false);
-        feedback.setText("Your final score is :"+points);
-
-    }
-
-
 }
