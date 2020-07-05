bb09d4e2d5be4efbf67b8987572839b73b556b2e
AliKemalUzar <alikemaluzar@gmail.com>
2020-07-05 14:54:21 +0200

    singleton + testdisease

--- src/program/Singleton.java
+++
@@ -1,9 +0,0 @@
-package program;
-
-public class Singleton {
-    public static void main(String[] args) {
-
-        SchrijvenController schrijvenController = SchrijvenController.getInstance();
-
-    }
-}
--- test/program/DiseasesControllerTest.java
+++ test/program/DiseasesControllerTest.java
@@ -7,18 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class DiseasesControllerTest {
-        //deze test kijd door middel van Decsion coverage of de aandoeningen van de medische dossier klopt.
 
+
+        //deze test kijd door middel van Decsion coverage of de aandoeningen van de medische dossier klopt.
         @Test
-        public void geval1Test() {
-            DiseasesController geval1Test = new DiseasesController();
-            assertEquals("Diabetes type 2, last bij beide knieën.", geval1Test.getGeval1());
-            assertEquals("Hartaandoening, veel last bij kransslagaders.", geval1Test.getGeval2());
+        public void LogischDiseseTest() {
+            DiseasesController diseasesController = new DiseasesController();
+            assertEquals("Diabetes type 2, last bij beide knieën.", diseasesController.getDiabetesGeval());
+            assertEquals("Hartaandoening, veel last bij kransslagaders.", diseasesController.getHartaandoeningGeval());
         }
         @Test
-        public void geval2Test(){
-            DiseasesController geval2Test = new DiseasesController();
-            assertNotEquals("Alzheimer", geval2Test.getGeval1());
-            assertNotEquals("Hooikoorts", geval2Test.getGeval2());
+        public void FysiekDiseaseTest(){
+            DiseasesController diseasesController = new DiseasesController();
+            assertNotEquals("Alzheimer", diseasesController.getDiabetesGeval());
+            assertNotEquals("Hooikoorts", diseasesController.getHartaandoeningGeval());
         }
     }
